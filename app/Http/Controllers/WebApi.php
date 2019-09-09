172839c539ac2bb83eb15a41f7f6663ce58c7edf
Butterflypig <346519079@qq.com>
2019-09-09 11:02:15 +0800

    api

---
+++ app/Http/Controllers/WebApi.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//引入模型
+use App\Models\Article;
+use App\Models\Setting;
+
+
+use Illuminate\Support\Facades\Input;
+
+use Illuminate\Http\Request;
+
+class WebApi extends Controller
+{
+
+    //获取菜单数据
+    public function getMenuData()
+    {
+        $article = Article::all();
+        return $article;
+    }
+
+    //获取文集数据
+    public function getArticleData()
+    {
+        $article = Article::where('type','text')->get();
+        return $article;
+    }
+
+    //获取分享数据
+    public function getShareData()
+    {
+        $Share = Article::where('type','share')->get();
+        return $Share;
+    }
+
+    //获取关于数据
+    public function getAboutData()
+    {
+        $setting = Setting::query()->findOrFail(1)->toArray();
+        return $setting;
+    }
+}
